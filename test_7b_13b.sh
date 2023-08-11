c327066ecce2db726f8ca486179f32ad71035ebb
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-08-11 13:16:32 +0800

    add files

--- test_7b_13b.sh
+++ test_7b_13b.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=0,1
 MODEL_DIR=$1
 OUT_DIR=$1
 
-torchrun --nproc_per_node 2 --master_port 7834 test.py \
+python3 -m torch.distributed.run --nproc_per_node 2 --master_port 7834 test.py \
                         --base_model $MODEL_DIR \
                         --data_path "./data/test_use.jsonl" \
                         --out_path $OUT_DIR \
