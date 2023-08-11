9766fe1f33c94b931e81b5b88dfe43052552d677
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-08-11 11:27:03 +0800

    add files

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --master_addr ${MAS
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 50 \
+    --save_steps 50000 \
     --save_total_limit 1 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
