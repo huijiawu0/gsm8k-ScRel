f630b0ac6537a034c46768fd2ccb3f9b9ce0e802
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-08-13 20:13:49 +0800

    add files

---
+++ calculate_length.py
@@ -0,0 +1,185 @@
+import json
+import logging
+from dataclasses import dataclass
+from json import JSONDecodeError
+from typing import Dict, Sequence
+
+import torch
+import transformers
+from datasets import Dataset
+from torch.utils.data import DataLoader
+
+from train import jload, preprocess, TrainingArguments, DataArguments, ModelArguments
+
+IGNORE_INDEX = -100
+DEFAULT_PAD_TOKEN = "[PAD]"
+DEFAULT_EOS_TOKEN = "</s>"
+DEFAULT_BOS_TOKEN = "</s>"
+DEFAULT_UNK_TOKEN = "</s>"
+PROMPT_DICT = {
+    "prompt_input": (
+        "Below is an instruction that describes a task, paired with an input that provides further context. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:"
+    ),
+    "prompt_no_input": (
+        "Below is an instruction that describes a task. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{instruction}\n\n### Response:"
+    ),
+}
+
+
+class SupervisedDataset(Dataset):
+    """Dataset for supervised fine-tuning."""
+    
+    def __init__(self, data_path: str, tokenizer: transformers.PreTrainedTokenizer):
+        super(SupervisedDataset, self).__init__()
+        logging.warning("Loading data...")
+        try:
+            list_data_dict = jload(data_path)
+        except BaseException:
+            with open(data_path, 'r') as f:
+                lines = f.readlines()
+            list_data_dict = []
+            list_data_length = []
+            for idx, line in enumerate(lines):
+                try:
+                    data_item = json.loads(line.strip())
+                    list_data_dict.append(data_item)
+        
+                    # Tokenize and print the length
+                    tokenized_length = len(tokenizer.encode(data_item['query']))
+                    list_data_length.append(tokenized_length)
+                    print(f"Line {idx} has a tokenized length of: {tokenized_length}")
+    
+                except JSONDecodeError:
+                    print(idx, line)
+                    continue
+        
+        # logging.warning("Formatting inputs...")
+        prompt_input, prompt_no_input = PROMPT_DICT["prompt_input"], PROMPT_DICT["prompt_no_input"]
+        # print(list_data_dict[0])
+        if 'instruction' in list_data_dict[0]:
+            pass
+        else:
+            def get_input(query):
+                if query.find('\n') == -1:
+                    return ''
+                return '\n'.join(query.split('\n')[1:])
+            
+            list_data_dict = [{'instruction': data['query'].split('\n')[0],
+                               'input': get_input(data['query']),
+                               'output': '%s\n#### %s' % (data['response'], data['answer'])}
+                              for data in list_data_dict]
+        # import ipdb; ipdb.set_trace()
+        sources = [
+            prompt_input.format_map(example) if example.get("input", "") != "" else prompt_no_input.format_map(example)
+            for example in list_data_dict
+        ]
+        targets = [f"{example['output']}{tokenizer.eos_token}" for example in list_data_dict]
+        
+        self.sources = sources
+        self.targets = targets
+        
+        # logging.warning("Tokenizing inputs... This may take some time...")
+        # data_dict = preprocess(sources, targets, tokenizer)
+        
+        # self.input_ids = data_dict["input_ids"]
+        # self.labels = data_dict["labels"]
+    
+    def __len__(self):
+        return len(self.sources)
+    
+    def naive__getitem__(self, i) -> Dict[str, torch.Tensor]:
+        return dict(input_ids=self.input_ids[i], labels=self.labels[i])
+    
+    def __getitem__(self, i):
+        return dict(input_ids=self.sources[i], labels=self.targets[i])
+
+
+@dataclass
+class DataCollatorForSupervisedDataset(object):
+    """Collate examples for supervised fine-tuning."""
+    
+    tokenizer: transformers.PreTrainedTokenizer
+    
+    def naive__call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
+        input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids", "labels"))
+        input_ids = torch.nn.utils.rnn.pad_sequence(
+            input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id
+        )
+        labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX)
+        return dict(
+            input_ids=input_ids,
+            labels=labels,
+            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
+        )
+    
+    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
+        sources = []
+        targets = []
+        for instance in instances:
+            source = instance['input_ids']
+            target = instance['labels']
+            sources.append(source)
+            targets.append(target)
+        
+        data_dict = preprocess(sources, targets, self.tokenizer)
+        input_ids, labels = data_dict['input_ids'], data_dict['labels']
+        # input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids", "labels"))
+        input_ids = torch.nn.utils.rnn.pad_sequence(
+            input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id
+        )
+        labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX)
+        return dict(
+            input_ids=input_ids,
+            labels=labels,
+            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
+        )
+
+
+def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer, data_args) -> Dict:
+    """Make dataset and collator for supervised fine-tuning."""
+    train_dataset = SupervisedDataset(tokenizer=tokenizer, data_path=data_args.data_path)
+    data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
+    return dict(train_dataset=train_dataset, eval_dataset=None, data_collator=data_collator)
+
+
+if __name__ == '__main__':
+    parser = transformers.HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=training_args.cache_dir,
+        model_max_length=training_args.model_max_length,
+        padding_side="right",
+        use_fast=False,
+    )
+
+    tokenizer.add_special_tokens(
+        {
+            "eos_token": DEFAULT_EOS_TOKEN,
+            "bos_token": DEFAULT_BOS_TOKEN,
+            "unk_token": DEFAULT_UNK_TOKEN,
+        }
+    )
+
+    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
+    train_dataset = data_module['train_dataset']
+    data_collator = data_module['data_collator']
+
+    batch_size = 32
+    dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=data_collator)
+
+    # 步骤 3: 查看每个批次
+    for batch in dataloader:
+        input_ids = batch['input_ids']
+        labels = batch['labels']
+        attention_mask = batch['attention_mask']
+    
+        print("Batch size:", input_ids.size(0))  # 批次的大小
+        print("Max sequence length in this batch:", input_ids.size(1))  # 批次中的最大序列长度（考虑了填充）
+        print("Attention Mask:", attention_mask)
+        print("----------")
